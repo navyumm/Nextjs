609727659a088ce860ed207e1446942a2d7414aa
Navyumm <Navyum12@gmail.com>
2024-04-28 17:13:31 +0530

    NextAuthjs done

---
+++ src/helpers/sendVerificationEmail.ts
@@ -0,0 +1,25 @@
+import { resend } from "@/lib/resend";
+import VerifvationEmail from "../../emails/VerificationEmail";
+import { ApiResponse } from "@/types/ApiResponse";
+
+export async function sendVerificationEmail(
+    email: string,
+    username: string,
+    verifyCode: string
+): Promise<ApiResponse> {
+    try {
+        await resend.emails.send({
+            from: 'onboarding@resend.dev',
+            to: email,
+            subject: 'Mystry message | Verification code',
+            react: VerifvationEmail({username, otp: verifyCode}),
+        });
+
+        return { success: false, message: "Verification email send successfully" }
+
+    } catch (emailError) {
+        console.error("Error sending verification email", emailError);
+        return { success: false, message: "Failed to send verification email" }
+
+    }
+}
